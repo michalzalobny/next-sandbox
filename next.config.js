38889337ffa438e8cc6a938e2268d63f7fde422b
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-02-14 00:03:04 +0100

    Update default locale

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
   compiler: {
     // ssr and displayName are configured by default
     styledComponents: true,
