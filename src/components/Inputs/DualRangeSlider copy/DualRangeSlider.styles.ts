272744d8ebca43d9c81b086f702aa562b7ef755e
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-02-18 12:12:52 +0100

    Update styling

--- src/components/Inputs/DualRangeSlider copy/DualRangeSlider.styles.ts
+++ src/components/Inputs/DualRangeSlider copy/DualRangeSlider.styles.ts
@@ -37,7 +37,7 @@ export const InputsWrapper = styled.span`
 
     &::-webkit-slider-thumb {
       appearance: none;
-      box-shadow: 0 0 20px rgba(255, 20, 20, 0.8);
+      box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
       height: 20px;
       width: 20px;
       border-radius: 50%;
