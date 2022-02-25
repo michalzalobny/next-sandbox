56f8670e52168fd1175ddcd1937aaf3b1484acb5
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2022-02-25 19:08:47 +0000

    Add select item

---
+++ src/components/Inputs/MultiSelect/components/SelectItem/SelectItem.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './SelectItem.styles';
+
+interface Props {}
+
+export const SelectItem = (props: Props) => {
+  return (
+    <>
+      <S.Wrapper>test</S.Wrapper>
+    </>
+  );
+};
